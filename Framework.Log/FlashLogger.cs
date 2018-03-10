bece949ebabea11155cecc27b9fb0d803cfb90ad
john.xiong <xiongnanbin@126.com>
2018-03-10 11:19:18 +0800

    add

---
+++ Framework.Log/FlashLogger.cs
@@ -0,0 +1,213 @@
+﻿/************************************************************
+ * 项目名称：Framework.Log
+ * 项目描述：
+ * 类名称：FlashLogger
+ * 版本号：
+ * 说明：
+ * 作者：Administrator
+ * 所在的域：JOHN-PC
+ * 命名空间：Framework.Log
+ * 注册组织：
+ * 机器名称：JOHN-PC
+ * CLR版本：4.0.30319.42000
+ * .NET Framework版本：4.5.2
+ * 创建时间：2017/7/9 16:16:28
+ * 更新时间：2017/7/9 16:16:28
+ * *********************************************************
+ * Copyright © Administrator 2017. All rights reserved
+ * ********************************************************/
+using Framework.Common.Helper;
+using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Framework.Log
+{
+    /// <summary>
+    /// 日志记录闪存辅助类
+    /// <para>将日志推送到内存队列，在通过读取队列将日志写入指定位置</para>
+    /// </summary>
+    public sealed partial class FlashLogger
+    {
+        /// <summary>
+        /// 记录消息Queue
+        /// </summary>
+        private readonly ConcurrentQueue<FlashLogMessage> _que;
+
+        /// <summary>
+        /// 信号
+        /// </summary>
+        private readonly ManualResetEvent _mre;
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        private readonly ILog _log;
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        private static FlashLogger _flashLog = new FlashLogger();
+
+
+        private FlashLogger()
+        {
+            var configFile = new FileInfo(ConfigHelper.MapPath(ConfigHelper.GetAppSetting("Log4netConfigFilePath")));
+            if (!configFile.Exists)
+            {
+                throw new Exception("未配置log4net配置文件！");
+            }
+
+            // 设置日志配置文件路径
+            XmlConfigurator.Configure(configFile);
+
+            _que = new ConcurrentQueue<FlashLogMessage>();
+            _mre = new ManualResetEvent(false);
+            _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        /// <summary>
+        /// 实现单例
+        /// </summary>
+        /// <returns></returns>
+        public static FlashLogger Instance()
+        {
+            return _flashLog;
+        }
+
+        /// <summary>
+        /// 另一个线程记录日志，只在程序初始化时调用一次
+        /// </summary>
+        public void Register()
+        {
+            Thread t = new Thread(new ThreadStart(WriteLog));
+            t.IsBackground = false;
+            t.Start();
+        }
+
+        /// <summary>
+        /// 从队列中写日志至磁盘
+        /// </summary>
+        private void WriteLog()
+        {
+            while (true)
+            {
+                // 等待信号通知
+                _mre.WaitOne();
+
+                FlashLogMessage msg;
+                // 判断是否有内容需要如磁盘 从列队中获取内容，并删除列队中的内容
+                while (_que.Count > 0 && _que.TryDequeue(out msg))
+                {
+                    // 判断日志等级，然后写日志
+                    switch (msg.Level)
+                    {
+                        case FlashLogLevel.Debug:
+                            _log.Debug(msg.Message, msg.Exception);
+                            break;
+                        case FlashLogLevel.Info:
+                            _log.Info(msg.Message, msg.Exception);
+                            break;
+                        case FlashLogLevel.Error:
+                            _log.Error(msg.Message, msg.Exception);
+                            break;
+                        case FlashLogLevel.Warn:
+                            _log.Warn(msg.Message, msg.Exception);
+                            break;
+                        case FlashLogLevel.Fatal:
+                            _log.Fatal(msg.Message, msg.Exception);
+                            break;
+                    }
+                }
+
+                // 重新设置信号
+                _mre.Reset();
+                Thread.Sleep(1);
+            }
+        }
+
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="message">日志文本</param>
+        /// <param name="level">等级</param>
+        /// <param name="ex">Exception</param>
+        public void EnqueueMessage(string message, FlashLogLevel level, Exception ex = null)
+        {
+            if ((level == FlashLogLevel.Debug && _log.IsDebugEnabled)
+             || (level == FlashLogLevel.Error && _log.IsErrorEnabled)
+             || (level == FlashLogLevel.Fatal && _log.IsFatalEnabled)
+             || (level == FlashLogLevel.Info && _log.IsInfoEnabled)
+             || (level == FlashLogLevel.Warn && _log.IsWarnEnabled))
+            {
+                _que.Enqueue(new FlashLogMessage
+                {
+                    Message = "[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss,fff") + "]\r\n" + message,
+                    Level = level,
+                    Exception = ex
+                });
+
+                // 通知线程往磁盘中写日志
+                _mre.Set();
+            }
+        }
+
+        public static void Debug(string msg, Exception ex = null)
+        {
+            Instance().EnqueueMessage(msg, FlashLogLevel.Debug, ex);
+        }
+
+        public static void Error(string msg, Exception ex = null)
+        {
+            Instance().EnqueueMessage(msg, FlashLogLevel.Error, ex);
+        }
+
+        public static void Fatal(string msg, Exception ex = null)
+        {
+            Instance().EnqueueMessage(msg, FlashLogLevel.Fatal, ex);
+        }
+
+        public static void Info(string msg, Exception ex = null)
+        {
+            Instance().EnqueueMessage(msg, FlashLogLevel.Info, ex);
+        }
+
+        public static void Warn(string msg, Exception ex = null)
+        {
+            Instance().EnqueueMessage(msg, FlashLogLevel.Warn, ex);
+        }
+
+    }
+
+    /// <summary>
+    /// 日志等级
+    /// </summary>
+    public enum FlashLogLevel
+    {
+        Debug,
+        Info,
+        Error,
+        Warn,
+        Fatal
+    }
+
+
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public class FlashLogMessage
+    {
+        public string Message { get; set; }
+        public FlashLogLevel Level { get; set; }
+        public Exception Exception { get; set; }
+
+    }
+}
---
+++ Framework.Log/ILogger.cs
@@ -0,0 +1,97 @@
+﻿/************************************************************
+ * 项目名称：Framework.Log
+ * 项目描述：
+ * 类名称：ILogger
+ * 版本号：
+ * 说明：
+ * 作者：Administrator
+ * 所在的域：JOHN-PC
+ * 命名空间：Framework.Log
+ * 注册组织：
+ * 机器名称：JOHN-PC
+ * CLR版本：4.0.30319.42000
+ * .NET Framework版本：4.5.2
+ * 创建时间：2017/7/9 16:28:36
+ * 更新时间：2017/7/9 16:28:36
+ * *********************************************************
+ * Copyright © Administrator 2017. All rights reserved
+ * ********************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Log
+{
+    /// <summary>
+    /// 日志记录接口
+    /// </summary>
+    public partial interface ILogger
+    {
+        /// <summary>
+        /// 信息
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        void Info(object message);
+
+        /// <summary>
+        /// 信息
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        /// <param name="e">异常</param>
+        void Info(object message, Exception e);
+
+        /// <summary>
+        /// 调试信息
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        void Debug(object message);
+
+        /// <summary>
+        /// 调试信息
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        /// <param name="e">异常</param>
+        void Debug(object message, Exception e);
+
+        /// <summary>
+        /// 警告
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        void Warn(object message);
+
+        /// <summary>
+        /// 警告
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        /// <param name="e">异常</param>
+        void Warn(object message, Exception e);
+
+        /// <summary>
+        /// 错误
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        void Error(object message);
+
+        /// <summary>
+        /// 错误
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        /// <param name="e">异常</param>
+        void Error(object message, Exception e);
+
+        /// <summary>
+        /// 致命错误
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        void Fatal(object message);
+
+        /// <summary>
+        /// 致命错误
+        /// </summary>
+        /// <param name="message">要记录的信息</param>
+        /// <param name="e">异常</param>
+        void Fatal(object message, Exception e);
+    }
+}
